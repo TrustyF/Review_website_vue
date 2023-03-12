9e6c4d603fb75112cd01689beecabcaec62b5a5c
Arthur <sirjacobs.arthur@gmail.com>
2023-03-12 15:35:37 +0100

    Change config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
-  publicPath:"/Review_website_vue/",
+  base:"/Review_website_vue/",
   transpileDependencies: true
 })
