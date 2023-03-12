5cfe8ad5b3485fba529d3ce50dc2ac50cafd4532
Arthur <sirjacobs.arthur@gmail.com>
2023-03-12 17:08:42 +0100

    Extracted firebase from code

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore, collection } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
-
-export const firebaseApp = initializeApp({
-    apiKey: process.env.VUE_APP_APIKEY,
-    authDomain: process.env.VUE_APP_AUTHDOMAIN,
-    projectId: process.env.VUE_APP_PROJECTID,
-    storageBucket: process.env.VUE_APP_STORAGEBUCKET,
-    messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
-    appId: process.env.VUE_APP_APPID,
-    measurementId: process.env.VUE_APP_MEASUREMENTID
-})
-
-// used for the firestore refs
-export const database = getFirestore(firebaseApp)
-export const img_database = getStorage(firebaseApp)
+// import { initializeApp } from 'firebase/app'
+// import { getFirestore, collection } from 'firebase/firestore'
+// import { getStorage } from 'firebase/storage'
+//
+// export const firebaseApp = initializeApp({
+//     apiKey: process.env.VUE_APP_APIKEY,
+//     authDomain: process.env.VUE_APP_AUTHDOMAIN,
+//     projectId: process.env.VUE_APP_PROJECTID,
+//     storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+//     messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+//     appId: process.env.VUE_APP_APPID,
+//     measurementId: process.env.VUE_APP_MEASUREMENTID
+// })
+//
+//
+// export const database = getFirestore(firebaseApp)
+// export const img_database = getStorage(firebaseApp)
 
 // here we can export reusable database references
 // export const moviesDb = collection(database, 'movies')
