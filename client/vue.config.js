85dac7a0c1f51008d914c86fcd892b41fad89971
Arthur <sirjacobs.arthur@gmail.com>
2023-03-12 12:19:27 +0100

    Cleanup remove firebase

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
+  base:"/Review_website_vue/",
   transpileDependencies: true
 })
 module.exports = {
