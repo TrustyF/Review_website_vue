9d66e38601117d3b4a43c714b963ea356bd79a89
Trusty <sirjacobs.arthur@gmail.com>
2023-03-19 17:53:27 +0100

    add requirements

--- server/main.py
+++ server/main.py
@@ -1,23 +1,19 @@
 from flask import Flask, request, Response
-# from flask_cors import CORS, cross_origingg
+from flask_cors import CORS
 
 import functions
 
 app = Flask(__name__)
-
-
-# CORS(app)
+CORS(app)
 
 
 # routes
 @app.route('/get_movies/', methods=["GET"])
-# @cross_origin()
 def get_movies():
     return functions.get_all_movies()
 
 
 @app.route('/edit_movie/', methods=["POST"])
-# @cross_origin()
 def edit_movie():
     functions.edit_movie(request.json)
     return Response(status=200)
