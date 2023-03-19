a3cd6ec54fd21b68f03e1c5c9a46af14f06b475c
Trusty <sirjacobs.arthur@gmail.com>
2023-03-19 17:44:11 +0100

    adding venv

--- server/main.py
+++ server/main.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, Response
-from flask_cors import CORS,cross_origin
-from pprint import pprint
+# from flask_cors import CORS, cross_origingg
 
 import functions
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
 
 
 # routes
 @app.route('/get_movies/', methods=["GET"])
-@cross_origin()
+# @cross_origin()
 def get_movies():
     return functions.get_all_movies()
 
 
 @app.route('/edit_movie/', methods=["POST"])
-@cross_origin()
+# @cross_origin()
 def edit_movie():
     functions.edit_movie(request.json)
     return Response(status=200)
