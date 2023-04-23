dadfc3c511e10e074a4f9a92980b9d7b883d2ee3
Trusty <sirjacobs.arthur@gmail.com>
2023-04-23 15:26:51 +0200

    Turned rating header into components

---
+++ src/components/Homepage/FilterMenu.vue
@@ -0,0 +1,136 @@
+<script setup>
+import {defineProps, defineEmits, ref, watch} from 'vue'
+import {Collapse} from 'vue-collapsed'
+import filterButton from '/public/assets/ui/filter_button.png'
+
+const props = defineProps(['props'])
+const emits = defineEmits(['filters'])
+const filters = {
+  'type': {
+    'name': 'Type',
+    'available': ["Movie", "Tv-series", "Documentary"],
+    'display': ["Movie", "Tv-series", "Documentary"],
+    'filter': [],
+  },
+  'format': {
+    'name': 'Format',
+    'available': ["Live-action", "Animated"],
+    'display': ["Filmed", "Animated"],
+    'filter': [],
+  },
+  'genre': {
+    'name': 'Genre',
+    'available': ["Action", "Adventure", "Crime", "Comedy", "Drama", "Family", "Horror", "Mystery", "Science Fiction", "Thriller"],
+    'display': ["Action", "Adventure", "Crime", "Comedy", "Drama", "Family", "Horror", "Mystery", "Science Fiction", "Thriller"],
+    'filter': [],
+  },
+  'rating': {
+    'name': 'Rating',
+    'available': ["9", "8", "7", "6", "5", "4", "3", "2", "1"],
+    'display': ["9★", "8★", "7★", "6★", "5★", "4★", "3★", "2★", "1★"],
+    'filter': [],
+  },
+  'length': {
+    'name': 'Length',
+    'available': ["0", "1", "2", "3"],
+    'display': ["-1 hour", "1-2 hours", "2-3 hours", "3+ hours"],
+    'filter': [],
+  },
+  'extra_settings': {
+    'exclude_mode': false
+  }
+}
+const state = ref(false)
+
+const excludeMode = ref(false)
+
+function swap_filter(filter, target) {
+  console.log('swapping filter')
+  if (target.includes(filter) === false) {
+    target.push(filter)
+  } else {
+    let index = target.indexOf(filter)
+    if (index > -1) { // only splice array when item is found
+      target.splice(index, 1); // 2nd parameter means remove one item only
+    }
+  }
+  emits('filters', filters)
+}
+
+</script>
+<template>
+
+  <div class="filter_button" @click="state = !state">
+    <div class="button_padding"></div>
+    <img :src="filterButton" alt="filter button" style="width: 15px;margin-right: 5px">
+    <h1>Filters</h1>
+  </div>
+
+  <Collapse :when="state" class="collapse">
+    <div class="filter_wrapper">
+      <div class="filters">
+        <div class="filter_types" v-for="elem in filters" :key="elem['name']">
+          <h1 style="text-decoration: underline;padding-bottom: 5px">{{ elem['name'] }}</h1>
+          <div v-for="(filter,index) in elem['available']" :key="filter" class="filter_content_list">
+            <label class="filter_label">
+              <input type="checkbox" style="cursor: pointer" @change="swap_filter(filter,elem['filter'])">
+              {{ elem['display'][index] }}
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Collapse>
+
+</template>
+
+<style scoped>
+.collapse {
+  transition: height 300ms ease-in-out;
+}
+.button_padding {
+  /*outline: 1px red solid;*/
+  position: absolute;
+  width: 90px;
+  height: 45px;
+  margin: 10px;
+  transform: translate(-20px,-25px);
+}
+.filter_wrapper {
+  padding: 25px 25px 5px 25px;
+}
+.filter_button {
+  cursor: pointer;
+  user-select: none;
+  margin: 20px 25px 0 25px;
+  display: flex;
+  flex-flow: row wrap;
+}
+
+.filters {
+  font-family: Calibri, serif;
+  user-select: none;
+  font-min-size: 1em;
+
+  height: 220px;
+  padding: 20px;
+
+  /*margin: 25px 25px 0 25px;*/
+  border-radius: 8px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+  overflow: hidden;
+
+  display: flex;
+  justify-content: left;
+  align-items: flex-start;
+  flex-flow: column wrap;
+  align-content: flex-start;
+
+  gap: 10px;
+}
+
+.filter_label {
+  font-size: 0.9em;
+  cursor: pointer;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,7 +2,8 @@
 import '../styles/globals.css'
 import MovieContainer from "@/components/MovieContainer";
 import DbHelper from "@/components/DbHelper";
-import HomeFilter from "@/components/Homepage/HomeFilter";
+import FilterMenu from "@/components/Homepage/FilterMenu";
+import RatingHeader from "@/components/Homepage/RatingHeader";
 
 import axios from 'axios'
 import {ref, onMounted} from 'vue'
@@ -16,6 +17,7 @@ const excludeMode = ref(false)
 const filters = ref({})
 
 const currentSelectedMovie = ref({})
+const currentSelectedRatings = ref([9,8,7,6,5,4,3,2,1])
 
 const servStatus = ref(0)
 const settingsOpen = ref(false)
@@ -29,7 +31,7 @@ onMounted(() => {
 
 function update_movies() {
   console.log('updating movies')
-  axios.post(`${local_api}/get_movies/`, filters.value)
+  axios.post(`${curr_api}/get_movies/`, filters.value)
       .then(response => {
         console.log(response)
         movies.value = response.data
@@ -37,19 +39,7 @@ function update_movies() {
       })
 }
 
-function getRankDetails(rank) {
-  if (rank === 10) return 'Perfect'
-  if (rank === 9) return 'Near perfect masterpiece'
-  if (rank === 8) return 'Extremely good'
-  if (rank === 7) return 'Quite good'
-  if (rank === 6) return 'Good with flaws'
-  if (rank === 5) return "Meh"
-  if (rank === 4) return 'Bad'
-  if (rank === 3) return 'Fucking bad'
-  if (rank === 2) return 'Holy shit bad'
-  if (rank === 1) return 'Affront to god'
-}
-function closeSettings(input){
+function closeSettings(input) {
   settingsOpen.value = input
 }
 
@@ -57,9 +47,13 @@ function reactCurrentMovie(input) {
   settingsOpen.value = true
   currentSelectedMovie.value = input
 }
-function reactFilters(input){
-  console.log('reacting to filter change',input)
+
+function reactFilters(input) {
+  console.log('reacting to filter change', input)
   filters.value = input
+
+  // currentSelectedRatings.value = input['rating']['filter']
+
   update_movies()
 }
 
@@ -67,22 +61,14 @@ function reactFilters(input){
 <template>
   <db-helper :inputData="currentSelectedMovie" :isOpen="settingsOpen" @settingsClosed="closeSettings"></db-helper>
 
-  <HomeFilter @filters="reactFilters"></HomeFilter>
+  <FilterMenu @filters="reactFilters"></FilterMenu>
 
   <div class="feed" v-if="servStatus===200">
-
-    <div class="movie_grid" v-for="rating in [9,8,7,6,5,4,3,2,1]" :key="rating">
-      <div class="rating_container">
-        <h1 class="rating_title">{{ rating }}</h1>
-        <p class="rating_desc"> {{ getRankDetails(rating) }}</p>
-      </div>
-
+    <div class="movie_grid" v-for="rating in currentSelectedRatings" :key="rating">
+    <rating-header :rating="rating"></rating-header>
       <div class="movie_container_wrapper" v-for="mov in movies[`ranked_${rating}`]" :key="mov.id">
-
         <MovieContainer :key="mov.id" :data="mov" @debug_current_movie_data="reactCurrentMovie"></MovieContainer>
-
       </div>
-
     </div>
   </div>
   <div class="feed" v-else>Loading...</div>
@@ -91,47 +77,22 @@ function reactFilters(input){
 <style scoped>
 .feed {
   /*outline: 1px solid blue;*/
-
   position: relative;
 
-
-  margin-left: 250px;
-  margin-right: 20px;
+  margin: 25px 15px 0 25px;
 
   display: flex;
   flex-direction: column;
 }
 
-.rating_container {
-  width: 100%;
-  user-select: none;
-}
-
-.rating_title {
-  font-family: "Arial Black", serif;
-  padding: 10px 5px 5px 0;
-  font-size: 2em;
-  border-bottom: solid black 1px;
-}
-
-.rating_desc {
-  /*font-family: Calibri;*/
-  font-size: 1em;
-  position: absolute;
-  transform: translate(35px, -25px);
-}
-
 .movie_grid {
-  /*outline: 1px solid red;*/
-
+  /*outline: 3px solid blue;*/
   width: 100%;
 
   display: flex;
   flex-wrap: wrap;
-  /*justify-content: space-between;*/
+  justify-content: center;
   flex-flow: row wrap;
   margin: auto;
 }
-
-
 </style>
