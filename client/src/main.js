88e0213268e59cf66b411500cebbc8ef3b1e4ef1
Trusty <sirjacobs.arthur@gmail.com>
2023-05-21 23:59:40 +0200

    Changed fade duration

--- client/src/main.js
+++ client/src/main.js
@@ -6,8 +6,8 @@ import VueLazyload from "vue-lazyload";
 
 const local_api = "http://localhost:5000"
 const server_api = "https://trustyfox.pythonanywhere.com"
-const devMode = true
-const sessionSeed = Math.random()
+let devMode = true
+let sessionSeed = Math.random()
 
 
 const app = createApp(App)
