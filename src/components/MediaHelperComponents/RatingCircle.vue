9ce88e4791a24b030f3684d7eb1f9a6a2b40225f
Trusty <sirjacobs.arthur@gmail.com>
2023-10-29 15:07:31 -0700

    Reworking media container

--- src/components/MediaHelperComponents/RatingCircle.vue
+++ src/components/MediaHelperComponents/RatingCircle.vue
@@ -11,8 +11,8 @@ const curr_api = inject("curr_api");
 
     <div class="progress-bar" :style="
     `background:
-      radial-gradient(closest-side, #2b2a34 80%, transparent 81%),
-      conic-gradient(${score > 5 ? score > 7.5 ? '#27ae60' : '#f1c40f' : '#e74c3c'} ${score * 10}%, #2b2a34 0);`
+      radial-gradient(closest-side, #2b2a34 80%, transparent 85%),
+      conic-gradient(${score >= 5 ? score >= 7 ? '#27ae60' : '#f1c40f' : '#e74c3c'} ${score * 10}%, ${'#595579'} 0);`
     ">
       <h1>{{ Math.round(score * 10) }}</h1>
       <h1 class="small_text">%</h1>
@@ -26,12 +26,13 @@ const curr_api = inject("curr_api");
 .rating_circle_wrapper{
   /*outline: 1px solid red;*/
   display: flex;
-  width: 100%;
+  height: 100%;
   aspect-ratio: 1/1;
 }
 .progress-bar {
   aspect-ratio: 1/1;
   border-radius: 50%;
+  box-shadow: 1px 2px 0 #1c1b23;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -40,12 +41,12 @@ h1 {
   /*position: absolute;*/
   color: white;
   font-weight: bold;
-  font-size: 1.8vw;
+  font-size: 0.8em;
   text-align: center;
-  transform: translate(0,0.5px);
+  transform: translate(0.5px,1px);
 }
 .small_text {
-  font-size: 1vw;
+  font-size: 0.5em;
   /*color: grey;*/
 }
 </style>
---
+++ src/components/MediaHelperComponents/TagBox.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {inject, onMounted, watch, ref, computed} from "vue";
+
+let props = defineProps(["score", "tag_img"]);
+const curr_api = inject("curr_api");
+
+</script>
+
+<template>
+  <div class="tag_box_wrapper">
+    <h1>{{ score }}</h1>
+    <img :src="tag_img" alt="tag_image" class="tag_image">
+  </div>
+</template>
+
+<style scoped>
+.tag_box_wrapper {
+  /*outline: 1px solid yellow;*/
+  padding: 0 5px 0 5px;
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  background-color: #41404d;
+  border-radius: 5px;
+  height: 70%;
+  font-size: 0.8em;
+  box-shadow: 1px 2px 0 #1c1b23;
+  margin: 3px;
+}
+.tag_image {
+  height: 70%;
+}
+</style>
