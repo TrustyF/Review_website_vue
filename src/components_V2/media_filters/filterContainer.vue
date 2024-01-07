593425ae459b803bdf40cea0baad5e9d928f5ee7
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-01-06 19:13:49 -0800

    Adding filters container

---
+++ src/components_V2/media_filters/filterContainer.vue
@@ -0,0 +1,52 @@
+<script setup>
+import {inject, onMounted, watch, ref, computed} from "vue";
+import FilterDropdown from "@/components_V2/media_filters/filterDropdown.vue";
+
+let props = defineProps(["media_type"]);
+let emits = defineEmits(["test"]);
+const curr_api = inject("curr_api");
+
+let genres = ref()
+let themes = ref()
+let tags = ref()
+
+async function fetch_filters() {
+
+  const url = new URL(`${curr_api}/media/get_filters`)
+  url.searchParams.set('type', props['media_type'])
+
+  const result = await fetch(url).then(response => response.json())
+  console.log('filters', result)
+
+  genres.value = result['genres']
+  themes.value = result['themes']
+  tags.value = result['tags']
+  tags.value.sort((a, b) => a['tier'] > b['tier'])
+
+}
+
+onMounted(() => {
+  fetch_filters()
+})
+</script>
+
+<template>
+  <div class="filter_wrapper">
+
+    <filter-dropdown :data="genres" title="Genres"></filter-dropdown>
+    <filter-dropdown :data="themes" title="Themes"></filter-dropdown>
+    <filter-dropdown :data="tags" title="Tags"></filter-dropdown>
+
+  </div>
+</template>
+
+<style scoped>
+.filter_wrapper {
+  display: flex;
+  width: 500px;
+  height: 500px;
+  background-color: black;
+
+  z-index: 100;
+}
+</style>
