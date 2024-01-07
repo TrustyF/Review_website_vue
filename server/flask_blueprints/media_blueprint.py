95f9fa84148437d101e7ca98f9153fd139b29774
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-01-06 19:14:10 -0800

    Adding getting filters

--- server/flask_blueprints/media_blueprint.py
+++ server/flask_blueprints/media_blueprint.py
@@ -10,7 +10,7 @@ from sqlalchemy.sql.expression import func
 from constants import MAIN_DIR, TMDB_ACCESS_TOKEN, TMDB_API_KEY
 from data_mapper.media_mapper import map_media
 from db_loader import db
-from sql_models.media_model import Media
+from sql_models.media_model import Media, Genre, Theme, Tag
 
 bp = Blueprint('media', __name__)
 
@@ -59,7 +59,7 @@ def get():
     media = query.all()
     mapped_media = map_media(media, media_type=media_type)
 
-    return mapped_media
+    return mapped_media, 200
 
 
 @bp.route("/get_image")
@@ -103,78 +103,21 @@ def search_extra_posters():
 
     return posters
 
-# @bp.route("/add_by_name")
-# def add_by_name():
-#     card_name = request.args.get('name')
-#     print(f'searching for {card_name}')
-#
-#     found_cards = search_card_by_name(card_name)
-#
-#     new_card = UserCard(card_template_id=found_cards[0].id)
-#
-#     db.session.add(new_card)
-#     db.session.commit()
-#     db.session.close()
-#
-#     return []
-#
-#
-# @bp.route("/delete")
-# def delete():
-#     card_id = request.args.get('id')
-#     print(f'deleting {card_id}')
-#
-#     # db.session.update(UserCard).where(id=card_id).values(is_deleted=1)
-#     db.session.query(UserCard).filter_by(id=card_id).delete()
-#     db.session.commit()
-#     db.session.close()
-#
-#     return []
-#
-#
-# @bp.route("/set_card_attrib")
-# def set_card_attrib():
-#     user_card_id = request.args.get('user_card_id')
-#     attr_name = request.args.get('attr_name')
-#     attribute = request.args.get('attribute')
-#
-#     print(f'updating card {attr_name} of card {user_card_id} to {attribute}')
-#
-#     if attribute == 'null':
-#         print('is null')
-#         db.session.query(UserCard).filter_by(id=user_card_id).update({str(attr_name): None})
-#     else:
-#         db.session.query(UserCard).filter_by(id=user_card_id).update({str(attr_name): attribute})
-#
-#     db.session.commit()
-#     db.session.close()
-#
-#     return []
-#
-#
-# @bp.route("/search_by_name")
-# def search_by_name():
-#     card_name = request.args.get('name')
-#
-#     if card_name == '':
-#         return []
-#
-#     print(f'searching for {card_name}')
-#
-#     found_cards = search_card_by_name(card_name)
-#     found_cards_ids = [x.id for x in found_cards]
-#
-#     query = (
-#         db.session
-#         .query(UserCard)
-#         .join(CardTemplate)
-#         .filter(UserCard.card_template_id.in_(found_cards_ids))
-#         # .filter(UserCard.storage_id.notin_([11]))
-#         .order_by(UserCard.storage_id, CARD_TYPE_PRIORITY, CardTemplate.name)
-#     )
-#
-#     cards_in_user = query.all()
-#
-#     mapped_cards = [map_card(uc) for uc in cards_in_user]
-#
-#     return mapped_cards
+
+@bp.route("/get_filters")
+def get_filters():
+    media_type = request.args.get('type')
+
+    print(f'getting filters for {media_type=}')
+
+    genres = db.session.query(Genre).all()
+    themes = db.session.query(Theme).all()
+    tags = db.session.query(Tag).all()
+
+    result = {
+        'genres': genres,
+        'themes': themes,
+        'tags': tags,
+    }
+
+    return result, 200
