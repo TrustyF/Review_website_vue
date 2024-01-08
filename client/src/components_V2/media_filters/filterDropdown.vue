da9f38bae335c472146b076064bebf8a7f15ce79
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-01-08 12:13:53 -0800

    Improved filter selectability, wip sending to backend

--- client/src/components_V2/media_filters/filterDropdown.vue
+++ client/src/components_V2/media_filters/filterDropdown.vue
@@ -6,19 +6,45 @@ let props = defineProps(["data", "title"]);
 let emits = defineEmits(["id"]);
 const curr_api = inject("curr_api");
 
+function clear_checkboxes() {
+  let checkboxes = document.getElementsByClassName('checkbox')
+  Array.from(checkboxes).forEach((elem) => {
+    if (elem.id.includes(props['title'])) {
+      elem.checked = false
+    }
+  })
+}
+
+function toggle_checkbox(id) {
+  let checkbox = document.getElementById(`checkbox${id}${props['title']}`)
+
+  if (checkbox.checked) {
+    checkbox.checked = false
+    emits('id', [id,false])
+  } else {
+    checkbox.checked = true
+    emits('id', [id,true])
+
+  }
+}
+
 </script>
 
 <template>
   <div class="filter_wrapper">
     <h1>{{ title }}</h1>
+    <button @click="clear_checkboxes">Clear</button>
 
     <div class="filter_list">
 
       <div class="filter_box" v-for="filter in data" :key="filter['id']">
-        <input id="checkbox" type="checkbox" @change="emits('id',filter['id'])" >
+        <input class="checkbox" :id="`checkbox${filter['id']}${title}`" type="checkbox">
 
-        <badge v-if="filter['image_path']!==undefined" :data="filter" :min_size="170" :show_title="true"></badge>
-        <label v-else for="checkbox">{{ filter['name'] }}</label>
+        <badge v-if="filter['image_path']!==undefined" :data="filter" :min_size="170"
+               :show_title="true"></badge>
+        <label v-else>{{ filter['name'] }}</label>
+        <div class="checkbox_click_box" @click="toggle_checkbox(filter['id'])"></div>
+
       </div>
 
     </div>
@@ -27,27 +53,39 @@ const curr_api = inject("curr_api");
 
 <style scoped>
 .filter_wrapper {
+  position: relative;
   display: flex;
   flex-flow: column;
   gap: 5px;
-  outline: 1px solid red;
+  /*outline: 1px solid red;*/
+  padding: 10px;
+  border: 1px solid #969696;
 }
+
 .filter_list {
   overflow-y: scroll;
   display: flex;
   flex-flow: column;
-  gap: 5px;
 }
+
 .filter_box {
-  /*outline: 1px solid greenyellow;*/
+  position: relative;
   display: flex;
   flex-flow: row;
 
   align-items: center;
   gap: 5px;
-
+  padding: 5px 0 5px 0;
 }
+
 .filter_image {
   height: 30px;
+}
+
+.checkbox_click_box {
+  /*outline: 1px solid greenyellow;*/
+  position: absolute;
+  width: 100%;
+  height: 100%;
 }
 </style>
