20c859bc55a2b52e12be1e4bee1885be67f7939e
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-03-20 17:25:17 -0700

    Increased refresh time

--- server/server_sleep_check.py
+++ server/server_sleep_check.py
@@ -4,4 +4,4 @@ import time
 while True:
     requests.get('https://review-trustyfox.pythonanywhere.com/media/sleep_check')
     requests.get('http://192.168.1.11:5000/media/sleep_check')
-    time.sleep(600)
+    time.sleep(3600)
