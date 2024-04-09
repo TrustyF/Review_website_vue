17d046f069e1ed6bcdf3e65f28e859d1a372334b
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-04-09 09:44:04 -0700

    Re-organized front page

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {computed,defineAsyncComponent, inject, onBeforeMount, onMounted, onUnmounted, ref} from "vue";
+import {computed, defineAsyncComponent, inject, onBeforeMount, onMounted, onUnmounted, ref} from "vue";
 
 import CreditsFooter from "../components/General/CreditsFooter.vue";
 import MediaScrollBanner from "../components/MediaBanner/MediaScrollBanner.vue";
@@ -13,15 +13,15 @@ const is_mobile = inject("is_mobile");
 const load_number = ref(-1)
 
 const genre_number_list = [
-    ['Action',80],
-    ['Adventure',79],
-    ['Sci-Fi',78],
-    ['Mystery',92],
-    ['Crime',85],
-    ['Animation',88],
-    ['Horror',89],
-    ['Comedy',87],
-    ['Romance',90],
+  ['Action', 80],
+  ['Adventure', 79],
+  ['Sci-Fi', 78],
+  ['Mystery', 92],
+  ['Crime', 85],
+  ['Animation', 88],
+  ['Horror', 89],
+  ['Comedy', 87],
+  ['Romance', 90],
 ]
 
 </script>
@@ -38,59 +38,100 @@ const genre_number_list = [
       <h2 style="font-size: 2em;
       filter:drop-shadow(3px 3px 2px black)drop-shadow(1px 1px 8px black)drop-shadow(1px 1px 15px black)">
         Welcome to Trusty's Corner !</h2>
-      <RouterLink style="color: grey;filter:drop-shadow(2px 2px 2px black);text-underline-offset: 2px;margin: -10px 0 -20px 10px;font-size: 1em" to="/info">What is this ?</RouterLink>
+      <RouterLink
+          style="color: grey;filter:drop-shadow(2px 2px 2px black);text-underline-offset: 2px;margin: -10px 0 -20px 10px;font-size: 1em"
+          to="/info">What is this ?
+      </RouterLink>
 
       <media-scroll-banner></media-scroll-banner>
     </div>
-    <div class="banner" style="margin-top: 5px">
+
+    <div class="banner" style="min-height: 341px;margin-top: 5px;">
+      <h1>Recommended movies</h1>
+      <media-banner :ratings=[7,10]
+                    :media_types="['movie']"
+                    v-show="load_number>=-1" @loaded="load_number+=1"></media-banner>
+    </div>
+
+    <div class="banner" style="min-height: 341px;">
       <h1>Watched recently</h1>
-      <media-banner :order="'date_added'" :media_types="['movie','tv']" v-show="load_number>=-1" @loaded="load_number+=1"></media-banner>
+      <media-banner :order="'date_added'"
+                    :media_types="['movie','tv']"
+                    v-show="load_number>=0" @loaded="load_number+=1"></media-banner>
     </div>
 
-    <div class="banner">
+    <div class="banner" style="min-height: 341px;">
       <h1>Latest releases</h1>
-      <media-banner :order="'release_date'" :media_types="['movie','tv']" v-show="load_number>=0" @loaded="load_number+=1"></media-banner>
+      <media-banner :order="'release_date'"
+                    :media_types="['movie','tv']"
+                    v-show="load_number>=1" @loaded="load_number+=1"></media-banner>
     </div>
 
-    <div class="banner">
-      <h1>Movies</h1>
-      <media-banner :ratings=[7,10] :media_types="['movie']" v-show="load_number>=1" @loaded="load_number+=1"></media-banner>
+    <div class="banner" style="min-height: 332px;">
+      <h1>TV</h1>
+      <media-banner :ratings=[6,10]
+                    :media_types="['Tv']"
+                    :size_override="[512, 720]"
+                    v-show="load_number>=2" @loaded="load_number+=1"></media-banner>
+    </div>
+
+    <div class="banner" v-for="(entry,index) in genre_number_list" :key="entry[1]" style="min-height: 341px;">
+      <h1>{{ entry[0] }}</h1>
+      <media-banner :ratings=[6,10]
+                    :media_types="['movie']"
+                    :genres="[entry[1]]"
+                    v-if="load_number>=2+index" @loaded="load_number+=1"></media-banner>
     </div>
 
     <div class="banner">
       <h1>Manga</h1>
-      <media-banner :ratings=[7,10] :media_types="['manga']" :size_override="[512, 720]" v-show="load_number>=2" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[7,10]
+                    :media_types="['manga']"
+                    :size_override="[512, 720]"></media-banner>
     </div>
 
     <div class="banner">
       <h1>Games</h1>
-      <media-banner :ratings=[7,10] :media_types="['game']" :size_override="[528, 704]" v-show="load_number>=3" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[7,10]
+                    :media_types="['game']"
+                    :size_override="[528, 704]"></media-banner>
     </div>
 
     <div class="banner">
       <h1>Youtube shows</h1>
-      <media-banner :ratings=[7,10] :media_types="['youtube']" :tier_lists="['tv-show']" :limit="10" :size_override="[910, 512]" v-if="load_number>=4" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[7,10]
+                    :media_types="['youtube']"
+                    :tier_lists="['tv-show']"
+                    :limit="10"
+                    :size_override="[910, 512]"></media-banner>
     </div>
 
     <div class="banner">
       <h1>Youtube documentaries</h1>
-      <media-banner :ratings=[7,10] :media_types="['youtube']" :tier_lists="['documentary']" :limit="10" :size_override="[910, 512]" v-if="load_number>=5" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[7,10]
+                    :media_types="['youtube']"
+                    :tier_lists="['documentary']"
+                    :limit="10"
+                    :size_override="[910, 512]"></media-banner>
     </div>
 
     <div class="banner">
       <h1>Youtube essays</h1>
-      <media-banner :ratings=[7,10] :media_types="['youtube']" :tier_lists="['essay']" :limit="10" :size_override="[910, 512]" v-if="load_number>=6" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[7,10]
+                    :media_types="['youtube']"
+                    :tier_lists="['essay']"
+                    :limit="10"
+                    :size_override="[910, 512]"></media-banner>
     </div>
 
-    <div class="banner" v-for="(entry,index) in genre_number_list" :key="entry[1]">
-      <h1>{{entry[0]}}</h1>
-      <media-banner :ratings=[6,10] :media_types="['movie']" :genres="[entry[1]]" v-if="load_number>=6+index" @loaded="load_number+=1"></media-banner>
-    </div>
+
 
 
     <div class="banner">
       <h1>Wall of shame</h1>
-      <media-banner :ratings=[1,4] :media_types="['movie','tv']" :limit="20" v-if="load_number>=16" @loaded="load_number+=1"></media-banner>
+      <media-banner :ratings=[1,4]
+                    :media_types="['movie','tv']"
+                    :limit="20"></media-banner>
     </div>
 
     <credits-footer></credits-footer>
@@ -102,6 +143,7 @@ const genre_number_list = [
 h1 {
   margin: 0 0 20px 0
 }
+
 .home_wrapper {
   display: flex;
   flex-flow: column;
@@ -113,9 +155,9 @@ h1 {
   min-height: 80vh;
   /*background-color: #1c1b23;*/
 }
+
 .banner {
   /*outline: 1px solid red;*/
-  min-height: 341px;
 }
 
 .top_banner {
@@ -128,6 +170,7 @@ h1 {
   .top_banner {
     margin-top: 20px;
   }
+
   .home_wrapper {
     padding: 100px 20px 50px 20px;
   }
