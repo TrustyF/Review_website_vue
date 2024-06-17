5a15b46f3c049e81dd39eaf55cae0cb550180630
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-06-17 14:32:56 -0700

    Improved all page, stats and added recent banners for manga and games

--- client/src/pages/AllPage.vue
+++ client/src/pages/AllPage.vue
@@ -1,11 +1,17 @@
 <script setup>
 import {ref, onMounted, inject, provide} from 'vue'
 import MediaFeed from "../components/media_feed/MediaFeed.vue";
-
+import MediaBanner from "../components/MediaBanner/MediaBanner.vue";
 
 </script>
 
 <template>
+  <div class="banner" style="padding-top: 100px">
+    <media-banner :ratings=[0,10]
+                  :order="'date_added'"
+                  :media_types="[]"
+                  :size_override="[528, 704]"></media-banner>
+  </div>
   <media-feed :media_types="['all']"
               :tier_lists="['all']"
               :media_container_type="'movie'"
