0a41f68b1f6a1c34f50e6346bae3b572461fed88
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-06-19 15:07:52 -0700

    Attempt to optimize request speed

--- server/app.py
+++ server/app.py
@@ -15,9 +15,9 @@ dev_mode = os.path.exists(os.path.join(MAIN_DIR, 'devmode.txt'))
 load_dotenv(os.path.join(MAIN_DIR, '.env'))
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    "pool_recycle": 280, "pool_timeout": 10, "pool_pre_ping": True
-}
+# app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+#     "pool_recycle": 280, "pool_timeout": 10, "pool_pre_ping": True
+# }
 
 db_username = os.getenv('MYSQL_DATABASE_USERNAME')
 db_password = os.getenv('MYSQL_DATABASE_PASSWORD')
