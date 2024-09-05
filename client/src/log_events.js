5f6c5a26cb916d1253c503fe37db7962a577ce63
Arthur Sirjacobs <sirjacobs.arthur@gmail.com>
2024-09-04 22:38:41 -0700

    Changed analytics backend

--- client/src/log_events.js
+++ client/src/log_events.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import {check_admin} from "@/firebase_auth.js";
-import {session_seed} from "@/session.js";
+import {geo_location, session_seed} from "@/session.js";
 
-let local_url = 'http://192.168.1.11:5000'
+// let local_url = 'http://192.168.1.11:5000'
 let server_url = ' https://analytics-trustyfox.pythonanywhere.com'
 
 export async function log_event(name, type, info) {
@@ -13,14 +13,18 @@ export async function log_event(name, type, info) {
     if (import.meta.env.DEV) console.log(name, {'info': String(info)}, session_seed)
 
     const url = `${server_url}/event/add`
-    const params = {
+
+    let params = {
         name: String(name),
         source: 'trusty_corner',
         type: String(type),
         info: String(info),
         uid: session_seed,
+        geo: geo_location,
     }
 
-    axios.get(url, {params: params})
+
+    axios.post(url, params)
         .then()
+        .catch(error => null)
 }
--- client/src/session.js
+++ client/src/session.js
@@ -1,1 +1,21 @@
+import axios from "axios";
+
 export const session_seed = Math.round(Math.random() * 10000000)
+
+let server_url = ' https://analytics-trustyfox.pythonanywhere.com'
+
+const get_geo = async () => {
+    let url = 'https://api.ipify.org?format=json';
+    return await axios.get(url)
+        .then(resp => {
+            // console.log(resp.data)
+            let geo_url = `${server_url}/event/geo_locate`
+            return axios.get(geo_url, {params: {ip: resp.data['ip']}})
+                .then(geo => geo.data)
+                .catch(err => err)
+
+        })
+        .catch(err => err)
+}
+
+export const geo_location = await get_geo()
